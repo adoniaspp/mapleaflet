5533f6d9fe325a2b28d28dba74375347ad2bdcc5
adoniaspp <adoniaspp@gmail.com>
2019-05-17 00:00:24 -0300

    map, markers and bottomsheet

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,112 @@ class LeafletMap extends StatefulWidget {
 class _LeafletMapState extends State<LeafletMap> {
   @override
   Widget build(BuildContext context) {
+    var listMarkers = <Marker>[
+      Marker(
+          width: 50,
+          height: 50,
+          point: LatLng(-1.449786, -48.488415),
+          builder: (context) => (Container(
+                  child: GestureDetector(
+                child: Icon(
+                  Icons.directions_bus,
+                  size: 50,
+                ),
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(),
+                  );
+                },
+              )))),
+      Marker(
+          width: 50,
+          height: 50,
+          point: LatLng(-1.451164, -48.486863),
+          builder: (context) => (Container(
+                  child: GestureDetector(
+                child: Icon(
+                  Icons.directions_bus,
+                  size: 50,
+                ),
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(),
+                  );
+                },
+              )))),
+      Marker(
+          width: 50,
+          height: 50,
+          point: LatLng(-1.446756, -48.487570),
+          builder: (context) => (Container(
+                  child: GestureDetector(
+                child: Icon(
+                  Icons.directions_bus,
+                  size: 50,
+                ),
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(),
+                  );
+                },
+              )))),
+      Marker(
+          width: 50,
+          height: 50,
+          point: LatLng(-1.450969, -48.494160),
+          builder: (context) => (Container(
+                  child: GestureDetector(
+                child: Icon(
+                  Icons.directions_bus,
+                  size: 50,
+                ),
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(),
+                  );
+                },
+              )))),
+      Marker(
+          width: 50,
+          height: 50,
+          point: LatLng(-1.444143, -48.493452),
+          builder: (context) => (Container(
+                  child: GestureDetector(
+                child: Icon(
+                  Icons.directions_bus,
+                  size: 50,
+                ),
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(),
+                  );
+                },
+              )))),
+    ];
     return MaterialApp(
       home: Scaffold(
         body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(51.5, -0.09),
-          zoom: 13.0
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: "https://api.tiles.mapbox.com/v4/"
-                "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
-            additionalOptions: {
-              'accessToken': 'pk.eyJ1IjoiYWRvbmlhc3BwIiwiYSI6ImNqdnEwaXkwMTAyZnAzeW54djVrOTJ6OGYifQ.TeYg1aiP3eYPAp5yzpP0VA',
-              'id': 'mapbox.streets',
-            },
+          options: MapOptions(
+            center: LatLng(-1.449786, -48.488415),
+            zoom: 18.0,
+            maxZoom: 20.0,
+            minZoom: 3.0,
           ),
-        ],
-      ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              subdomains: ['a', 'b', 'c'],
+            ),
+            MarkerLayerOptions(
+              markers: listMarkers,
+            )
+          ],
+        ),
       ),
     );
   }
